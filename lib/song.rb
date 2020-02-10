f0a356cc8d89f1dffd210813d03a348681ecc3d9
Khalid T <khalid.tissire@gmail.com>
2020-02-10 07:44:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,37 +1,54 @@
 class Song
+  attr_accessor :name, :artist, :genre
+
+  @@all= []
   @@count = 0
   @@genres = []
   @@artists = []
-  
-  attr_accessor :name, :artist, :genre
-  
-  def initialize (name, artist, genre)
+  @@genres_stat = {}
+  @@artists_stat = {}
+
+  def initialize(name,artist,genre)
     @name = name
     @artist = artist
     @genre = genre
-
     @@count += 1
+    @@all << self
     @@genres << genre
     @@artists << artist
   end
-  
-  def self.count
-    @@count
-  end
-  
-  def self.genres
-    @@genres.uniq
-  end
-  
-  def self.artists
-    @@artists.uniq
-  end
-  
-  def self.genre_count
-    @@genres.inject(Hash.new(0)) { |hash, element| hash[element] += 1; hash }
+
+  def Song.count
+    return @@count
   end
 
-  def self.artist_count
-    @@artists.inject(Hash.new(0)) { |hash, element| hash[element] += 1; hash}
-    end
+  def Song.genres
+    return @@genres.uniq
+  end
+
+  def Song.artists
+    return @@artists.uniq
+  end
+
+  def Song.genre_count
+    @@all.each { |s|
+      if @@genres_stat[s.genre.to_sym]
+        @@genres_stat[s.genre.to_sym] += 1
+      else
+        @@genres_stat[s.genre.to_sym] = 1
+      end
+    }
+    return @@genres_stat
+  end
+
+  def Song.artist_count
+    @@all.each { |s|
+      if @@artists_stat[s.artist.to_sym]
+        @@artists_stat[s.artist.to_sym] += 1
+      else
+        @@artists_stat[s.artist.to_sym] = 1
+      end
+    }
+    return @@artists_stat
+  end
 end
